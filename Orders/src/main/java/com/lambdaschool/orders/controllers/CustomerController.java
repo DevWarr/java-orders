92731b9b5b265e49010b37294fa20c3d278e35b5
Devin Warrick <dev.warr@gmail.com>
2019-07-17 23:33:16 -0500

    getCustomerByName() works properly!

--- Orders/src/main/java/com/lambdaschool/orders/controllers/CustomerController.java
+++ Orders/src/main/java/com/lambdaschool/orders/controllers/CustomerController.java
@@ -2,13 +2,16 @@ package com.lambdaschool.orders.controllers;
 
 import com.lambdaschool.orders.model.Customer;
 import com.lambdaschool.orders.services.CustomerService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 
 @RestController
@@ -24,5 +27,17 @@ public class CustomerController
     {
         List<Customer> list = customerService.findAll();
         return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/name/{custname}",
+                produces = {"application/json"})
+    public ResponseEntity<?> getCustomerByName(@PathVariable String custname)
+    {
+        Customer c = customerService.findCustomerByName(custname);
+        if (c == null)
+        {
+            throw new EntityNotFoundException(custname);
+        }
+        return new ResponseEntity<>(c, HttpStatus.OK);
     }
 }
